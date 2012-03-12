3afa34b515004b13de674b9ae2da720a1c8cc7f6
jt1134 <jt1134@gmail.com>
2012-03-12 02:22:39 -0500

    simplify updater.sh

--- updater.sh
+++ updater.sh
@@ -1,44 +1,72 @@
 #!/tmp/busybox sh
 #
-# CyanogenMod Filesystem Converter for Samsung Infuse4G
+# Filsystem Conversion Script for Samsung Infuse4G
+# (c) 2012 by Teamhacksung
 #
 
 set -x
 export PATH=/:/sbin:/system/xbin:/system/bin:/tmp:$PATH
 
-test_mount()
-{
-    echo "Trying to unmount $1 on $2"
-    /tmp/busybox umount -l /$1
-    if [ $1 == "system" ]; then
-        # always go ahead and format /system
-           umount_format $1 $2
-    elif ! /tmp/busybox mount -t ext4 /dev/block/$2 /$1; then
-           umount_format $1 $2
-	else 
-           echo "Partition $1 is already EXT4."
-           /tmp/busybox umount -l /$1
+# unmount everything
+/tmp/busybox umount -l /system
+/tmp/busybox umount -l /cache
+/tmp/busybox umount -l /data
+/tmp/busybox umount -l /datadata
+
+# create directories
+/tmp/busybox mkdir -p /system
+/tmp/busybox mkdir -p /cache
+/tmp/busybox mkdir -p /data
+/tmp/busybox mkdir -p /datadata
+
+# make sure sdcard is mounted
+if ! /tmp/busybox grep -q /sdcard /proc/mounts ; then
+    /tmp/busybox mkdir -p /sdcard
+    /tmp/busybox umount -l /dev/block/mmcblk0p1
+    if ! /tmp/busybox mount -t vfat /dev/block/mmcblk0p1 /sdcard ; then
+        /tmp/busybox echo "Cannot mount sdcard."
+        exit 1
     fi
-}
+fi
 
-umount_format()
-{
-        if [ $(/tmp/busybox mount | grep "/dev/block/$2 on /$1 type" | wc -l) -eq "0" ]; then
-                echo "Formatting $1 on $2 to EXT4"
-                /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -a /$1 /dev/block/$2
-                echo "Successfully converted $1 on $2"
-        else
-                # We're still mounted for some reason
-                echo "Cannot unmount $1 on $2"
-                exit 1 
-        fi
-}
+# remove old log
+rm -rf /sdcard/cyanogenmod.log
 
+# everything is logged into /sdcard/cyanogenmod.log
+exec >> /sdcard/cyanogenmod.log 2>&1
 
-# check each partition
-test_mount system stl9
-test_mount datadata stl10
-test_mount data mmcblk0p2
-test_mount cache stl11 
-/tmp/busybox mount -t ext4 /dev/block/stl11 /cache
+#
+# filesystem conversion
+#
+
+# format system if not ext4
+if ! /tmp/busybox mount -t ext4 /dev/block/stl9 /system ; then
+    /tmp/busybox umount /system
+    /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -a /system /dev/block/stl9
+fi
+
+# format datadata if not ext4
+if ! /tmp/busybox mount -t ext4 /dev/block/stl10 /datadata ; then
+    /tmp/busybox umount /datadata
+    /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -a /datadata /dev/block/stl10
+fi
+
+# format cache if not ext4
+if ! /tmp/busybox mount -t ext4 /dev/block/stl11 /cache ; then
+    /tmp/busybox umount /cache
+    /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -a /cache dev/block/stl11
+fi
+
+# format data if not ext4
+if ! /tmp/busybox mount -t ext4 /dev/block/mmcblk0p2 /data ; then
+    /tmp/busybox umount /data
+    /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -a /data /dev/block/mmcblk0p2
+fi
+
+# unmount everything
+/tmp/busybox umount -l /system
+/tmp/busybox umount -l /cache
+/tmp/busybox umount -l /data
+/tmp/busybox umount -l /datadata
+
 exit 0
