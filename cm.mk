0989968d38094ba15b3d90dcd61934d7cdf5c152
scotthartbti <scott.hart.bti@gmail.com>
2012-08-29 16:55:14 -0700

    Use new square bootanimation Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    Change-Id: I51828b237fd6ec5a647f7df5e33824039f9d2486

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Release name
 PRODUCT_RELEASE_NAME := Infuse4G
 
-TARGET_BOOTANIMATION_NAME := vertical-480x800
+TARGET_BOOTANIMATION_NAME := 480
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
