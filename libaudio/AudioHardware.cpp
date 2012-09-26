06e135a0bb4606e655cb5042dbb84342cfbfcdf5
scotthartbti <scott.hart.bti@gmail.com>
2012-09-26 15:10:49 -0700

    Revert "Add support for Dock Audio"
    This reverts commit 1a346017d4a6ace08bc41e70f337de3c53724c30.

--- libaudio/AudioHardware.cpp
+++ libaudio/AudioHardware.cpp
@@ -630,7 +630,6 @@ void AudioHardware::setVoiceVolume_l(float volume)
                 break;
 
             case AudioSystem::DEVICE_OUT_SPEAKER:
-case AudioSystem::DEVICE_OUT_ANLG_DOCK_HEADSET:
                 ALOGD("### speaker call volume");
                 type = SOUND_TYPE_SPEAKER;
                 break;
@@ -775,7 +774,6 @@ status_t AudioHardware::setIncallPath_l(uint32_t device)
                     break;
 
                 case AudioSystem::DEVICE_OUT_SPEAKER:
-case AudioSystem::DEVICE_OUT_ANLG_DOCK_HEADSET:
                     ALOGD("### incall mode speaker route");
                     path = SOUND_AUDIO_PATH_SPEAKER;
                     break;
@@ -1048,9 +1046,6 @@ const char *AudioHardware::getOutputRouteFromDevice(uint32_t device)
     switch (device) {
     case AudioSystem::DEVICE_OUT_EARPIECE:
         return "RCV";
-case AudioSystem::DEVICE_OUT_ANLG_DOCK_HEADSET:
-if (mMode == AudioSystem::MODE_RINGTONE) return "RING_SPK";
-else return "EXTRA_DOCK_SPEAKER";
     case AudioSystem::DEVICE_OUT_SPEAKER:
         if (mMode == AudioSystem::MODE_RINGTONE) return "RING_SPK";
         else return "SPK";
@@ -1062,7 +1057,6 @@ else return "EXTRA_DOCK_SPEAKER";
         else return "HP";
     case (AudioSystem::DEVICE_OUT_SPEAKER|AudioSystem::DEVICE_OUT_WIRED_HEADPHONE):
     case (AudioSystem::DEVICE_OUT_SPEAKER|AudioSystem::DEVICE_OUT_WIRED_HEADSET):
-case (AudioSystem::DEVICE_OUT_SPEAKER|AudioSystem::DEVICE_OUT_ANLG_DOCK_HEADSET):
         if (mMode == AudioSystem::MODE_RINGTONE) return "RING_SPK_HP";
         else return "SPK_HP";
     case AudioSystem::DEVICE_OUT_BLUETOOTH_SCO:
@@ -1080,7 +1074,6 @@ const char *AudioHardware::getVoiceRouteFromDevice(uint32_t device)
     case AudioSystem::DEVICE_OUT_EARPIECE:
         return "RCV";
     case AudioSystem::DEVICE_OUT_SPEAKER:
-case AudioSystem::DEVICE_OUT_ANLG_DOCK_HEADSET:
         return "SPK";
     case AudioSystem::DEVICE_OUT_WIRED_HEADPHONE:
     case AudioSystem::DEVICE_OUT_WIRED_HEADSET:
