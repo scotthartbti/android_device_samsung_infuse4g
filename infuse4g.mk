ef22663c38307a50a37fc4e1818cdce9481a5efe
scotthartbti <scott.hart.bti@gmail.com>
2012-11-25 05:02:24 -0800

    More 4.2 changes.  Getting there. Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    Change-Id: I0b2bd51095fc79045f1bca6c32c8bcf7a6cbdaaf

--- infuse4g.mk
+++ infuse4g.mk
@@ -164,6 +164,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
        mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
        persist.sys.vold.switchexternal=1
 
+# dpi
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=240
+
+# enable Google-specific location features,
+# like NetworkLocationProvider and LocationCollector
+PRODUCT_PROPERTY_OVERRIDES += \
+        ro.com.google.locationfeatures=1 \
+        ro.com.google.networklocation=1
+
 # Extended JNI checks
 # The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
 # before they have a chance to cause problems.
