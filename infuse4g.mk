2e051c575781d6c0983ff8a6ae43bd28a9b57d9e
scotthartbti <scott.hart.bti@gmail.com>
2012-11-30 18:03:16 -0800

    Remove libnetcmdiface Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    Change-Id: Iafbe539c004890118e3459cff3cb6417a648c235

--- infuse4g.mk
+++ infuse4g.mk
@@ -210,9 +210,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
    device/samsung/infuse4g/zram.sh:root/sbin/zram.sh
 
-# wifi
-PRODUCT_PACKAGES += libnetcmdiface
-
 # See comment at the top of this file. This is where the other
 # half of the device-specific product definition file takes care
 # of the aspects that require proprietary drivers that aren't
