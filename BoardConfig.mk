52cfe41e7ad6bad4eef7220e847db0685c09540e
scotthartbti <scott.hart.bti@gmail.com>
2013-05-05 00:51:43 -0400

    Use releasetools extensions from aries-common Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,8 +43,7 @@ TARGET_RECOVERY_INITRC := device/samsung/aries-common/recovery.rc
 BOARD_MOBILEDATA_INTERFACE_NAME = "pdp0"
 
 # Releasetools
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/aries-common/releasetools/aries_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/aries-common/releasetools/aries_img_from_target_files
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/aries-common
 
 TARGET_PROVIDES_LIBAUDIO := true
 
