599867e9c67604947e0c7c5141c444762d36a6d2
scotthartbti <scott.hart.bti@gmail.com>
2013-05-23 23:14:12 -0400

    add libnetcmdiface Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- infuse4g.mk
+++ infuse4g.mk
@@ -98,7 +98,8 @@ PRODUCT_PACKAGES += \
 	audio.usb.default \
 	sensors.aries \
 	lights.aries \
-	power.s5pc110
+	power.s5pc110 \
+	libnetcmdiface
 
 PRODUCT_COPY_FILES += \
 	device/samsung/infuse4g/libaudio/audio_policy.conf:system/etc/audio_policy.conf
